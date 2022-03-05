d1b7f1cb44126cf14d4aec048bf907bd915a4c50
makhtar fall <makhtarfall2003@gmail.com>
2022-03-05 10:26:05 +0100

    ran migration for the offer model and added bootstrap to application

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_03_03_194255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offers", force: :cascade do |t|
+    t.string "departing_from"
+    t.string "going_to"
+    t.integer "price_per_km"
+    t.integer "available_seats"
+    t.string "car_model"
+    t.string "car_photo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
